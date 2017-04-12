eda945f77d67b172899ab48fb5900fe4d42e2843
Nicolas Bertrand <$EMAIL>
2017-04-12 10:42:43 +0200

    slice update

--- third/slice.go
+++ third/slice.go
@@ -11,11 +11,16 @@ func main () {
                 "titi",
         }
         fmt.Println(test)
+        sl := test[0:1]
 
-        sl := test[:4]
 
         fmt.Println(sl)
-        fmt.Printf("slice type: %T, value: %V\n",sl, sl)
-        fmt.Printf("array type: %T, value: %V\n",test, test)
+//      fmt.Printf("slice type: %T, value: %V\n",sl, sl)
+//        fmt.Printf("array type: %T, value: %V\n",test, test)
+        fmt.Printf("slice lgth: %d, capacity: %d, content %v\n", len(sl), cap(sl), sl)
+        sl = sl[0:3]
 
+        fmt.Printf("slice lgth: %d, capacity: %d, content %v\n", len(sl), cap(sl), sl)       
+         sl = test[1:4]
+               fmt.Printf("slice lgth: %d, capacity: %d, content %v\n", len(sl), cap(sl), sl)       
 }
