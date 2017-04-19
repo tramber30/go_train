af0245cb694ff9dc6916c1c0c7929b0bdd916e63
Nicolas Bertrand <$EMAIL>
2017-04-19 15:20:05 +0200

    pic exercise

---
+++ third/pic.go
@@ -0,0 +1,16 @@
+package main
+
+import {
+        "fmt"
+        "golang.org/x/tour/pic"
+}
+
+func Pic(dx, dy int) [][]uint8 {
+        pow := make (
+        for i, value := range
+
+
+func main () {
+        pic.Show(Pic)
+
+}
